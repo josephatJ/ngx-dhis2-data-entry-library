b0ebc02bc654047712b0d3143c4622794c615543
josephatJ <josephatjulius24@gmail.com>
2019-09-21 19:14:19 +0300

    update the returned status and input back on the custom component

--- projects/ngx-dhis2-custom-forms-entry/src/lib/ngx-dhis2-custom-forms-entry.module.ts
+++ projects/ngx-dhis2-custom-forms-entry/src/lib/ngx-dhis2-custom-forms-entry.module.ts
@@ -1,11 +1,11 @@
-import { NgModule } from '@angular/core';
-import { NgxDhis2CustomFormsEntryComponent } from './ngx-dhis2-custom-forms-entry.component';
-import { CustomFormComponent } from './components/custom-form/custom-form.component';
+import { NgModule } from "@angular/core";
+import { NgxDhis2CustomFormsEntryComponent } from "./ngx-dhis2-custom-forms-entry.component";
+import { CustomFormComponent } from "./components/custom-form/custom-form.component";
+import { CommonModule } from "@angular/common";
 
 @NgModule({
   declarations: [NgxDhis2CustomFormsEntryComponent, CustomFormComponent],
-  imports: [
-  ],
+  imports: [CommonModule],
   exports: [NgxDhis2CustomFormsEntryComponent]
 })
-export class NgxDhis2CustomFormsEntryModule { }
+export class NgxDhis2CustomFormsEntryModule {}
